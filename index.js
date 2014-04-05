2512faf3340e50e780b0e8c5d5d02054a5a19ba5
Mr. Wiredancer <jiahao@dxhackers.com>
2014-04-05 13:27:52 +0800

    added comment

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ var http = require('http');
 module.exports = function(){
     var next, generateNext, handler, upperNext;
 
+    //初始化next, 构造一个closure来保存nextIndex变量(记住对middleware数组的具体调用)
     var generateNext = function(req, res){
         var nextIndex = 0; 
 
         return function(err){
             if (nextIndex >= handler.stack.length){
+                
+                //此app是被作为middleware调用的，需要调用上一层的next
                 if (upperNext){
                     upperNext(err);
+    
+                //已经没有middleware可以调用，返回404或500
                 }else{
                     res.statusCode = err?500:404;
                     res.end();
@@ -17,17 +22,18 @@ module.exports = function(){
                 }
             }
 
-
             var middleware = handler.stack[nextIndex]
                 , isErrMiddleware = (middleware.length === 4);
             
             nextIndex += 1;
             
+            //根据err的有无选择是否跳过当前middleware
             if ((err && !isErrMiddleware) || (!err && isErrMiddleware)){
                 next(err);
                 return;
             }
-            
+
+            //调用middleware, 捕捉任何可能的异常
             try{
                 if (err){
                     middleware(err, req, res, next);
@@ -43,11 +49,10 @@ module.exports = function(){
 
     handler = function(req, res, parentNext){
         if (parentNext){
-            upperNext = parentNext;
-
+            upperNext = parentNext;//此app作为middleware被调用，保存parent的next以备之后调用
         }
 
-        next = generateNext(req, res);
+        next = generateNext(req, res); //初始化next
         next();
     }
 
